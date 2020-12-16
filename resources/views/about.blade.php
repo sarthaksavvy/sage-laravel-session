b34a966a9ea1dea9508ce06edd89c3789747776d
sarthaksavvy <bitfumes@gmail.com>
2020-12-16 16:40:44 +0530

    learned routing and views

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>About</title>
 </head>
 <body>
-<a href="/">Home</a>
+    <a href="/">Home</a>
     <a href="about">About</a>
-    <h1>Hello Sage from Laravel, Home Page</h1>
+    <h1>This is about page</h1>
 </body>
 </html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contact</title>
 </head>
 <body>
-<a href="/">Home</a>
-    <a href="about">About</a>
-    <h1>Hello Sage from Laravel, Home Page</h1>
+    <h1>This is contact page</h1>
 </body>
 </html>
